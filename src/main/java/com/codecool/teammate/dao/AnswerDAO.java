83a7e62a2ad6398ee21454b5891b2833c438b5ab
martaJesztak <jesztakm@gmail.com>
2018-06-21 14:29:55 +0200

    AnswerDAO interface created.

---
+++ src/main/java/com/codecool/teammate/dao/AnswerDAO.java
@@ -0,0 +1,13 @@
+package com.codecool.teammate.dao;
+
+import com.codecool.teammate.model.Answer;
+
+import java.util.List;
+
+public interface AnswerDAO {
+    void add (Answer answer);
+    Answer find (int id);
+    void remove(int id);
+
+    List<Answer> findAllQuestionByQuestion(int id);
+}
