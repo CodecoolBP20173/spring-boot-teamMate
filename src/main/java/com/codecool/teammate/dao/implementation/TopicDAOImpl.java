aeefa4822e783dc26b3152256caf17a4f83f6d10
Lukács Bence <lukacsb@gmail.com>
2018-06-21 13:19:56 +0200

    modified pom.xml

--- src/main/java/com/codecool/teammate/dao/implementation/TopicDAOImpl.java
+++ src/main/java/com/codecool/teammate/dao/implementation/TopicDAOImpl.java
@@ -32,39 +32,42 @@ public class TopicDAOImpl implements TopicDAO {
     @Override
     public void add(Topic topic) {
         EntityManager em = getEntityManager();
+
         em.persist(topic);
+
         em.close();
     }
 
     @Override
     public Topic find(int id) {
         EntityManager em = getEntityManager();
-        Topic topicToFind = em.find(Topic.class, id);
-        em.close();
 
+        Topic topicToFind = em.find(Topic.class, id);
+
+        em.close();
         return topicToFind;
     }
 
     @Override
     public void remove(int id) {
         EntityManager em = getEntityManager();
+
         Topic topicToRemove = find(id);
         if (topicToRemove != null) {
             em.remove(topicToRemove);
         }
+
         em.close();
     }
 
     @Override
     public List<Topic> findAllTopic() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("teammatePU");
-        EntityManager em = emf.createEntityManager();
+        EntityManager em = getEntityManager();
 
-        Query query = em.createQuery("SELECT t FROM ");
+        Query query = em.createQuery("SELECT t FROM Topic t");
         List<Topic> resultList = query.getResultList();
 
         em.close();
-        emf.close();
         return resultList;
     }
 
