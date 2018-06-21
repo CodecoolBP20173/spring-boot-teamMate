d14399e5ec7d432bbb196555137efd5488b92909
Dóra Surmann <surmann.dora@gmail.com>
2018-06-21 11:13:51 +0200

    basic functions in topic dao implemented

--- src/main/java/com/codecool/teammate/dao/implementation/TopicDAOImpl.java
+++ src/main/java/com/codecool/teammate/dao/implementation/TopicDAOImpl.java
@@ -2,11 +2,9 @@ package com.codecool.teammate.dao.implementation;
 
 import com.codecool.teammate.dao.TopicDAO;
 import com.codecool.teammate.model.Topic;
+import com.sun.istack.internal.NotNull;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.*;
 import java.util.List;
 
 public class TopicDAOImpl implements TopicDAO {
@@ -19,23 +17,33 @@ public class TopicDAOImpl implements TopicDAO {
 
     @Override
     public void add(Topic topic) {
-
+        EntityManager em = getEntityManager();
+        em.persist(topic);
+        em.close();
     }
 
     @Override
     public Topic find(int id) {
-        return null;
+        EntityManager em = getEntityManager();
+        Topic topicToFind = em.find(Topic.class, id);
+        em.close();
+
+        return topicToFind;
     }
 
     @Override
     public void remove(int id) {
-
+        EntityManager em = getEntityManager();
+        Topic topicToRemove = find(id);
+        if (topicToRemove != null) {
+            em.remove(topicToRemove);
+        }
+        em.close();
     }
 
     @Override
     public List<Topic> findAllTopic() {
         EntityManager em = getEntityManager();
-
         Query query = em.createQuery("SELECT t FROM Topic t");
         List<Topic> resultList = query.getResultList();
         em.close();
