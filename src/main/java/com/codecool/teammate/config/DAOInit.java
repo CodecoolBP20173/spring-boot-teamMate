64bb043a26c413a4c2e8aa71bd9041fade4c14f7
martaJesztak <jesztakm@gmail.com>
2018-07-03 13:26:24 +0200

    Implement dependency injection to Customer DAO

--- src/main/java/com/codecool/teammate/config/DAOInit.java
+++ src/main/java/com/codecool/teammate/config/DAOInit.java
@@ -1,17 +1,22 @@
 package com.codecool.teammate.config;
 
 import com.codecool.teammate.dao.implementation.AnswerDAOImpl;
+import com.codecool.teammate.dao.implementation.CustomerDAOImpl;
 import com.codecool.teammate.dao.implementation.QuestionDAOImpl;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DAOInit {
     // Todo - EMF and EM
     private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("teammatePU");
+    private static EntityManager em = emf.createEntityManager();
 
-    QuestionDAOImpl questionDAO = new QuestionDAOImpl(emf.createEntityManager());
-    AnswerDAOImpl answerDAO = new AnswerDAOImpl(emf.createEntityManager());
+
+    QuestionDAOImpl questionDAO = new QuestionDAOImpl(em);
+    AnswerDAOImpl answerDAO = new AnswerDAOImpl(em);
+    CustomerDAOImpl customerDAO = new CustomerDAOImpl(em);
 
     public DAOInit() {
     }
@@ -21,4 +26,6 @@ public class DAOInit {
     }
 
     public AnswerDAOImpl getAnswerDAO() { return answerDAO; }
+
+    public CustomerDAOImpl getCustomerDAO() { return customerDAO; }
 }
