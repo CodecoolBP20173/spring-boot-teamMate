dab010bdc6c166dcd1997e28ab999762f797b429
martaJesztak <jesztakm@gmail.com>
2018-07-03 13:32:23 +0200

    Implement dependency injection to Review DAO

--- src/main/java/com/codecool/teammate/config/DAOInit.java
+++ src/main/java/com/codecool/teammate/config/DAOInit.java
@@ -1,8 +1,10 @@
 package com.codecool.teammate.config;
 
+import com.codecool.teammate.dao.ReviewDAO;
 import com.codecool.teammate.dao.implementation.AnswerDAOImpl;
 import com.codecool.teammate.dao.implementation.CustomerDAOImpl;
 import com.codecool.teammate.dao.implementation.QuestionDAOImpl;
+import com.codecool.teammate.dao.implementation.ReviewDAOImpl;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -17,15 +19,16 @@ public class DAOInit {
     QuestionDAOImpl questionDAO = new QuestionDAOImpl(em);
     AnswerDAOImpl answerDAO = new AnswerDAOImpl(em);
     CustomerDAOImpl customerDAO = new CustomerDAOImpl(em);
+    ReviewDAOImpl reviewDAO = new ReviewDAOImpl(em);
 
     public DAOInit() {
     }
 
-    public QuestionDAOImpl getQuestionDAO() {
-        return questionDAO;
-    }
+    public QuestionDAOImpl getQuestionDAO() { return questionDAO; }
 
     public AnswerDAOImpl getAnswerDAO() { return answerDAO; }
 
     public CustomerDAOImpl getCustomerDAO() { return customerDAO; }
+
+    public ReviewDAOImpl getReviewDAO() { return reviewDAO; }
 }
