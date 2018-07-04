706f3055458ef8ce494d697472ac723b472c95b8
martaJesztak <jesztakm@gmail.com>
2018-07-04 13:19:10 +0200

    Mockito and test directory added.

---
+++ src/test/com/codecool/teammate/dao/implementation/QuestionDAOImplTest.java
@@ -0,0 +1,19 @@
+package com.codecool.teammate.dao.implementation;
+
+import com.codecool.teammate.config.DAOInit;
+import com.codecool.teammate.model.Question;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class QuestionDAOImplTest {
+
+    @Test
+    public void findAllQuestionBySubstring() {
+        
+
+    }
+}
