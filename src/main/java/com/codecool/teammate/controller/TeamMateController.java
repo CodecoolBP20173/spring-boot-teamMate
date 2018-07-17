e0164e26c06db38059c3fad7c7e379b32cfae580
Dóra Surmann <surmann.dora@gmail.com>
2018-07-17 11:38:51 +0200

    index page handler created in controller

--- src/main/java/com/codecool/teammate/controller/TeamMateController.java
+++ src/main/java/com/codecool/teammate/controller/TeamMateController.java
@@ -1,7 +1,35 @@
 package com.codecool.teammate.controller;
 
+
+import com.codecool.teammate.repository.QuestionRepository;
+import com.codecool.teammate.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class TeamMateController {
+
+    @Autowired
+    private TopicRepository topicRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @GetMapping("/")
+    public String index(
+            @RequestParam("searched_string") String searchedString,
+            ModelMap modelMap) {
+
+        if (searchedString != null) {
+            modelMap.addAttribute("searched_string");
+            modelMap.addAttribute("searched_question", questionRepository.findQuestionsByTitleContaining(searchedString) );
+        } else {
+            modelMap.addAttribute("topics", topicRepository.findAll());
+        }
+        return "index";
+    }
 }
