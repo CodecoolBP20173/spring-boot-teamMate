d3c7a1386235d2fce7de34157678103811c2510f
Dóra Surmann <surmann.dora@gmail.com>
2018-08-02 12:25:19 +0200

    clean code mofifications

--- src/main/java/com/codecool/teammate/controller/ReviewController.java
+++ src/main/java/com/codecool/teammate/controller/ReviewController.java
@@ -7,7 +7,6 @@ import com.codecool.teammate.model.Review;
 import com.codecool.teammate.repository.AnswerRepository;
 import com.codecool.teammate.repository.QuestionRepository;
 import com.codecool.teammate.repository.ReviewRepository;
-import com.codecool.teammate.repository.TopicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -35,22 +34,20 @@ public class ReviewController {
             (@RequestParam("review_input") String reviewInput,
              @RequestParam("question_id") String idStr,
              RedirectAttributes redirectAttributes) {
-        System.out.println(reviewInput);
 
         String regex = "\\d+";
         if (idStr != null && idStr.matches(regex)) {
             int question_id = Integer.parseInt(idStr);
             Question question = questionRepository.findById(question_id);
-            Answer answer = answerRepository.findById(question.getAnswer().getId());
+            Answer answer = question.getAnswer();
 
             if (answer.getReviews().isEmpty()) {
-                reviewRepository.save(new Review(reviewInput, question, answer));
+                reviewRepository.save(new Review(reviewInput, answer));
             } else {
                 List<Review> reviews = answer.getReviews();
-                Review review = new Review();
-                review.setDescription(reviewInput);
-                reviews.add(review);
-                reviewRepository.save(review);
+                Review newReview = new Review(reviewInput, answer);
+                reviews.add(newReview);
+                reviewRepository.save(newReview);
             }
 
             redirectAttributes.addAttribute("id", idStr);
@@ -67,21 +64,16 @@ public class ReviewController {
             if (idStr != null && idStr.matches(regex)) {
                 Integer id = Integer.parseInt(idStr);
                 Question question = questionRepository.findById(id);
-                List <Review> reviews = question.getAnswer().getReviews();
                 modelMap.addAttribute("id", idStr);
 
                 if (question != null) {
                     modelMap.addAttribute("question", question);
-                    if (question.getAnswer() != null) {
-                        modelMap.addAttribute("answer_description", question.getAnswer().getDescription());
-                        if (!question.getAnswer().getReviews().isEmpty()) {
-                            for (Review review : reviews) {
-                                modelMap.addAttribute("review_description", review.getDescription());
-                            }
-                        }
-                    }
+                    Answer answer = question.getAnswer();
 
-                    return "review";
+                    if (answer != null) {
+                        modelMap.addAttribute("answer", answer);
+                        return "review";
+                    }
                 }
             }
             return "redirect:/topics";
