cef89f34b9437ed6fa05fe0327b8b831359f5ed5
Dóra Surmann <surmann.dora@gmail.com>
2018-08-02 12:22:14 +0200

    new methods added for finding review

--- src/main/java/com/codecool/teammate/repository/ReviewRepository.java
+++ src/main/java/com/codecool/teammate/repository/ReviewRepository.java
@@ -4,7 +4,11 @@ import com.codecool.teammate.model.Answer;
 import com.codecool.teammate.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // This is how you define basic CRUD operations on the Customer entity
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    Review findById(int id);
+    List<Review> findAllByAnswerId(int id);
 }
